2da73d114a8e363b7a9ef528469151c57c182df1
Alex Savi <alex.savi.kv@gmail.com>
2025-12-15 17:46:14 +0530

    simple changes done

--- About/script.js
+++ About/script.js
@@ -74,28 +74,27 @@
     if (!('ontouchstart' in window)) {
       window.addEventListener('mousemove', mouseMove);
     }
-    window.addEventListener('scroll', scrollCheck);
+    // window.addEventListener('scroll', scrollCheck); // Disabled to keep animation running
     window.addEventListener('resize', resize);
   }
 
   function mouseMove(e) {
     var posx = posy = 0;
-    if (e.pageX || e.pageY) {
-      posx = e.pageX;
-      posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY) {
-      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    if (e.clientX || e.clientY) {
+      posx = e.clientX;
+      posy = e.clientY;
+    } else if (e.pageX || e.pageY) {
+      posx = e.pageX - (document.body.scrollLeft + document.documentElement.scrollLeft);
+      posy = e.pageY - (document.body.scrollTop + document.documentElement.scrollTop);
     }
     target.x = posx;
     target.y = posy;
   }
 
-  function scrollCheck() {
-    if (document.body.scrollTop > height) animateHeader = false;
-    else animateHeader = true;
-  }
+  // function scrollCheck() {
+  //   if (document.body.scrollTop > height) animateHeader = false;
+  //   else animateHeader = true;
+  // }
 
   function resize() {
     width = window.innerWidth;
